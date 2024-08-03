954b76a2bccc16a2426741ae19e62efe9e9632ca
Ngoc Do <ngocdo@maruweb.vn>
2024-08-03 23:18:14 +0700

    Update substr

---
+++ substr.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+ 
+int main() {
+    string str = "Hello World!";
+ 
+    string substr = str.substr(6);
+ 
+    cout << substr << endl;
+ 
+    return 0;
+}
