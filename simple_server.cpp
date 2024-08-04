5fda13444e7c2a0a78f1ebd5b1d0a25c75b8f6fd
Ngoc Do <ngocdo@maruweb.vn>
2024-08-04 22:14:40 +0700

    Test deploy httplib

--- simple_server.cpp
+++ simple_server.cpp
@@ -1,5 +1,15 @@
+#include <iostream>
+#include <string>
+#include <curl/curl.h>
 #include <httplib.h>
+using namespace std;
 using namespace httplib;
+
+static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
 
 int main(void) {
     // HTTP
@@ -10,10 +20,24 @@ int main(void) {
     });
 
     svr.Get("/test-crawl", [&](const Request & /*req*/, Response &res) {
-        Client cli("https://www.google.com");
-        auto crawl = cli.Get("/");
+        CURL *curl;
+        CURLcode curlRes;
+        std::string readBuffer;
 
-        res.set_content(crawl->body, "text/html");
+        curl = curl_easy_init();
+        if (curl) {
+            curl_easy_setopt(curl, CURLOPT_URL, "http://www.google.com");
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+            curlRes = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+
+            // std::cout << readBuffer << std::endl;
+
+            res.set_content(readBuffer, "text/html");
+        } else {
+            res.set_content("LOL", "text/html");
+        }
     });
 
     svr.listen("localhost", 1234);
